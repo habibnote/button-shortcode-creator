8f11bc4dd9a758af65daa137a32d41adda2245d9
Habibur Rahman <habibblog97@gmail.com>
2023-12-09 10:42:49 +0600

    register post custom post type done

--- app/Admin.php
+++ app/Admin.php
@@ -11,6 +11,13 @@ class Admin {
      * Class constructor
      */
     public function __construct() {
-        
+        add_action( 'init', [$this, 'register_post_type'] );
+    }
+
+    /**
+     * Register Custom post type
+     */
+    public function register_post_type() {
+        include_once( BSC_DIR . "/views/admin/bsc-post.php" );
     }
 }
