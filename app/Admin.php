2777b958628f4384c9a13e4cfd2d583a6460cd38
Habibur Rahman <habibblog97@gmail.com>
2023-12-09 14:56:02 +0600

    admin ajax done

--- app/Admin.php
+++ app/Admin.php
@@ -13,6 +13,18 @@ class Admin {
     public function __construct() {
         add_action( 'init', [$this, 'register_post_type'] );
         add_action( 'admin_enqueue_scripts', [$this, 'admin_enqueue_scripts'] );
+        add_action( 'wp_ajax_bsc_add_button', [$this, 'bsc_add_button'] );
+    }
+
+    /**
+     * Process ajax requrest
+     */
+    public function bsc_add_button() {
+        $post_id = $_POST['post_id'];
+        
+        echo $post_id;
+
+        die;
     }
 
     /**
@@ -29,5 +41,12 @@ class Admin {
         wp_enqueue_style( 'admin', BSC_ASSET . '/admin/css/admin.css', '', time(), 'all' );
 
         wp_enqueue_script( 'admin', BSC_ASSET . '/admin/js/admin.js', ['jquery', 'wp-color-picker'], time(), true );
+
+        $ajax_url = admin_url( 'admin-ajax.php' );
+        $nonce    = wp_create_nonce( 'bsc_nonce_admin' );
+        wp_localize_script( 'admin', 'BSC', array( 
+            'ajax'  => $ajax_url,
+            'admin_nonce'=> $nonce,
+        ) );
    }   
 }
--- assets/admin/js/admin.js
+++ assets/admin/js/admin.js
@@ -1,15 +1,34 @@
 jQuery(document).ready(function ($) {
+
     // Add color picker to all elements with class 'color-picker'
     $('.color-picker').wpColorPicker({
         defaultColor: '#000000'
     });
 
+    //add new btn
     $(document).on('click', '.bsc-add-new', function() {
-        // Clone the parent div
-        var clonedDiv = $(this).closest('.bsc-btn-meta.single-btn').clone();
+        
+        let post_id = $(this).attr('post_id');
+
+        //clone a btn
+        let clonedDiv = $(this).closest('.bsc-btn-meta.single-btn').clone();
   
         // Append the cloned div after the original div
         $(this).closest('.bsc-btn-meta.single-btn').after(clonedDiv);
+
+        $.ajax({
+            type: 'POST',
+            url: BSC.ajax,
+            data: {
+                action: 'bsc_add_button',
+                nonce: BSC.admin_nonce,
+                post_id: post_id,
+            },
+            success: function(response) {
+                console.log( response );
+            }
+        });
+
       });
   
     //   Delete Button Click Event
