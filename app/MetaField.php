792010aa4844992c6669c8aa9c372e683e5c4963
habibnote <habibblog97@gmail.com>
2023-12-11 11:43:09 +0600

    btn setting separated

--- app/MetaField.php
+++ app/MetaField.php
@@ -37,73 +37,7 @@ class MetaField {
                                     <input type="text" name="bsc_btn_url[]" value="<?php echo $bsc_btn_info['bsc_btn_url'][$i] ?? ''; ?>" />
                                 </p>
                             </div>
-                            <div class="bsc-btn-space-field">
-                                
-                                <div class="single-field">
-                                    <label>Padding: <span>px</span></label>
-                                    <input type="number" name="bsc_btn_padding[]" value="<?php echo $bsc_btn_info['bsc_btn_padding'][$i] ?? ''; ?>" />
-                                </div>
-                                <div class="single-field">
-                                    <label>Margin: <span>px</span> </label>
-                                    <input type="number" name="bsc_btn_margin[]" value="<?php echo $bsc_btn_info['bsc_btn_margin'][$i] ?? ''; ?>" />
-                                </div>
-                            </div>
-                            <div class="bsc-btn-color-field">
-                                <div class="single-field">
-                                    <label>Color: </label>
-                                    <input type="text" class="color-picker" name="bsc_btn_color[]" value="<?php echo $bsc_btn_info['bsc_btn_color'][$i] ?? ''; ?>">
-                                </div>
-                                <div class="single-field">
-                                    <label>Background: </label>
-                                    <input type="text" class="color-picker" name="bsc_btn_background[]" value="<?php echo $bsc_btn_info['bsc_btn_background'][$i] ?? ''; ?>">
-                                </div>
-                                <div class="single-field">
-                                    <label>Border-color: </label>
-                                    <input type="text" class="color-picker" name="bsc_btn_border-color[]" value="<?php echo $bsc_btn_info['bsc_btn_border-color'][$i] ?? ''; ?>">
-                                </div>
-                            </div>
-                            <div class="bsc-btn-color-field">
-                                <div class="single-field">
-                                    <label>Hover Color: </label>
-                                    <input type="text" class="color-picker" name="bsc_btn_hover_color[]" value="<?php echo $bsc_btn_info['bsc_btn_hover_color'][$i] ?? ''; ?>">
-                                </div>
-                                <div class="single-field">
-                                    <label>Hover Background: </label>
-                                    <input type="text" class="color-picker" name="bsc_btn_hover_bg_color[]" value="<?php echo $bsc_btn_info['bsc_btn_hover_bg_color'][$i] ?? ''; ?>">
-                                </div>
-                            </div>
-                            <div class="bsc-btn-color-field">
-                                <div class="single-field">
-                                    <label>Font Size: <span>px</span> </label>
-                                    <input type="text" name="bsc_btn_font_size[]" value="<?php echo $bsc_btn_info['bsc_btn_font_size'][$i] ?? ''; ?>" />
-                                </div>
-                                <div class="single-field">
-                                    <label>Font Weight: </label>
-                                    <select name="bsc_btn_font_weight[]">
-                                        <?php 
-                                            $bsc_btn_font_weights = ['normal', 'bold']; 
-                                            foreach( $bsc_btn_font_weights as $weight ) {
-                                                printf( "<option value='%s' %s>%s</option>", $weight, bsc_is_selected( $bsc_btn_info['bsc_btn_font-weight'][$i], $weight  ), $weight );
-                                            } 
-                                        ?>
-                                    </select>
-                                </div>
-                                <div class="single-field">
-                                    <label>Font Style: </label>
-                                    <select name="bsc_btn_font-style[]">
-                                        <option value="normal">normal</option>
-                                        <option value="italic">Italic</option>
-
-                                        <?php 
-                                            $bsc_btn_font_style = ['normal', 'italic']; 
-                                            foreach( $bsc_btn_font_style as $style ) {
-                                                printf( "<option value='%s' %s>%s</option>", $style, bsc_is_selected( $bsc_btn_info['bsc_btn_font-style'][$i], $style ), $style );
-                                            } 
-                                        ?>
-
-                                    </select>
-                                </div>
-                            </div>
+                            <?php ( new self() )->btn_setting_meta_field( $bsc_btn_info ); ?>
                         </div>
                         <div class="right-area">
                             <div class="btn-right-area">
@@ -118,5 +52,83 @@ class MetaField {
             ?>
         </p>
         <?php 
+    }
+
+    /**
+     * setting metafield
+     */
+    public function btn_setting_meta_field( $bsc_btn_info ) {
+        ?>
+            <div class="bsc_setting_metafield">
+
+                <div class="bsc-btn-space-field">
+                    
+                    <div class="single-field">
+                        <label>Padding: <span>px</span></label>
+                        <input type="number" name="bsc_btn_padding[]" value="<?php echo $bsc_btn_info['bsc_btn_padding'][$i] ?? ''; ?>" />
+                    </div>
+                    <div class="single-field">
+                        <label>Margin: <span>px</span> </label>
+                        <input type="number" name="bsc_btn_margin[]" value="<?php echo $bsc_btn_info['bsc_btn_margin'][$i] ?? ''; ?>" />
+                    </div>
+                </div>
+                <div class="bsc-btn-color-field">
+                    <div class="single-field">
+                        <label>Color: </label>
+                        <input type="text" class="color-picker" name="bsc_btn_color[]" value="<?php echo $bsc_btn_info['bsc_btn_color'][$i] ?? ''; ?>">
+                    </div>
+                    <div class="single-field">
+                        <label>Background: </label>
+                        <input type="text" class="color-picker" name="bsc_btn_background[]" value="<?php echo $bsc_btn_info['bsc_btn_background'][$i] ?? ''; ?>">
+                    </div>
+                    <div class="single-field">
+                        <label>Border-color: </label>
+                        <input type="text" class="color-picker" name="bsc_btn_border-color[]" value="<?php echo $bsc_btn_info['bsc_btn_border-color'][$i] ?? ''; ?>">
+                    </div>
+                </div>
+                <div class="bsc-btn-color-field">
+                    <div class="single-field">
+                        <label>Hover Color: </label>
+                        <input type="text" class="color-picker" name="bsc_btn_hover_color[]" value="<?php echo $bsc_btn_info['bsc_btn_hover_color'][$i] ?? ''; ?>">
+                    </div>
+                    <div class="single-field">
+                        <label>Hover Background: </label>
+                        <input type="text" class="color-picker" name="bsc_btn_hover_bg_color[]" value="<?php echo $bsc_btn_info['bsc_btn_hover_bg_color'][$i] ?? ''; ?>">
+                    </div>
+                </div>
+                <div class="bsc-btn-color-field">
+                    <div class="single-field">
+                        <label>Font Size: <span>px</span> </label>
+                        <input type="text" name="bsc_btn_font_size[]" value="<?php echo $bsc_btn_info['bsc_btn_font_size'][$i] ?? ''; ?>" />
+                    </div>
+                    <div class="single-field">
+                        <label>Font Weight: </label>
+                        <select name="bsc_btn_font_weight[]">
+                            <?php 
+                                $bsc_btn_font_weights = ['normal', 'bold']; 
+                                foreach( $bsc_btn_font_weights as $weight ) {
+                                    printf( "<option value='%s' %s>%s</option>", $weight, bsc_is_selected( $bsc_btn_info['bsc_btn_font-weight'][$i], $weight  ), $weight );
+                                } 
+                            ?>
+                        </select>
+                    </div>
+                    <div class="single-field">
+                        <label>Font Style: </label>
+                        <select name="bsc_btn_font-style[]">
+                            <option value="normal">normal</option>
+                            <option value="italic">Italic</option>
+
+                            <?php 
+                                $bsc_btn_font_style = ['normal', 'italic']; 
+                                foreach( $bsc_btn_font_style as $style ) {
+                                    printf( "<option value='%s' %s>%s</option>", $style, bsc_is_selected( $bsc_btn_info['bsc_btn_font-style'][$i], $style ), $style );
+                                } 
+                            ?>
+                        </select>
+                    </div>
+                </div>
+            </div>
+        <?php
+
     }
 }
